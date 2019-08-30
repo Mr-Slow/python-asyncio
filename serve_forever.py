90249d9406e676a4a75c627cd0950aafa1849a6b
yuaner <lydbdx@163.com>
2019-08-30 09:14:19 +0800

    20190830 09:14

--- serve_forever.py
+++ serve_forever.py
@@ -1,12 +1,15 @@
 import asyncio
 
 async def client_connected(reader, writer):
-    await reader.readline()
+    msg = await reader.readline()
+    print(msg)
 
 async def main(host, port):
     srv = await asyncio.start_server(
         client_connected, host, port)
     # await srv.serve_forever()
     await srv.start_serving()
+    srv.close()
+    await srv.wait_closed()
 
-asyncio.run(main('127.0.0.1', 0))
+asyncio.run(main('127.0.0.1', 8888))
